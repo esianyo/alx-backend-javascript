9f0fdb3eb119a1ac43db937948fc995e08c5a237
esianyo <esianicd@gmail.com>
2024-03-10 18:54:22 +0000

    modified to prevent variables from being overwritten

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -5,6 +5,10 @@ export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
     const task = true; // variable modified to constant
     const task2 = false; // variable modified to constant
+
+    if (task && task2) {
+      return [task, task2];
+    }
   }
 
   return [task, task2];
