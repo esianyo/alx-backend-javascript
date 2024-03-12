59f0595b61be00c2a03d0741621b50b491057b59
esianyo <esianicd@gmail.com>
2024-03-12 20:26:59 +0000

    getting API response

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+function getResponseFromAPI() {
+    return new Promise((resolve) => {
+      // Simulating an asynchronous API call
+      setTimeout(() => {
+        const data = "Response from API"; // Replace with actual API response
+        resolve(data); // Resolve the promise with the API response
+      }, 2000); // Simulated delay of 2 seconds
+    });
+  }
+  
+  export default getResponseFromAPI;
