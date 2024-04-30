cbba0900ec0d770bd5b068458964b9ab7f5d89ad
esianyo <esianicd@gmail.com>
2024-04-30 22:43:45 +0000

    NodeJS Basics

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,19 +1,74 @@
 // 7-http_express.js
 
 const express = require('express');
-const { countStudents } = require('./3-read_file_async');
 
 const app = express();
+
+const fs = require('fs');
+
+function countStudents(path) {
+  function makePromise() {
+    return new Promise((resolve, reject) => {
+      fs.readFile(path, 'utf-8', (error, data) => {
+        if (error) {
+          return reject(new Error('Cannot load the database'));
+        }
+        let dataArray = data.split('\n');
+        dataArray = dataArray.filter((str) => str !== '');
+
+        const newArray = [];
+        const fieldCount = {};
+        const fieldNames = {};
+
+        for (let i = 0; i < dataArray.length; i += 1) {
+          dataArray[i] = dataArray[i].split(',');
+          if (i !== 0) {
+            const obj = {};
+            for (let j = 0; j < dataArray[0].length; j += 1) {
+              obj[dataArray[0][j]] = dataArray[i][j];
+            }
+            const { field } = obj;
+            let count = 0;
+            if (fieldCount[field]) {
+              count = fieldCount[field];
+            }
+            fieldCount[field] = count + 1;
+            if (!fieldNames[field]) {
+              fieldNames[field] = [];
+            }
+            const names = fieldNames[field];
+            names.push(obj.firstname);
+            fieldNames[field] = names;
+            newArray.push(obj);
+          }
+        }
+        let allLines = `Number of students: ${newArray.length}`;
+        Object.keys(fieldCount).forEach((field) => {
+          allLines += `\nNumber of students in ${field}: ${fieldCount[field]}. List:${fieldNames[field].map((firstname) => ` ${firstname}`)}`;
+        });
+        return resolve(allLines);
+      });
+    });
+  }
+  return makePromise();
+}
+
+let path = process.argv[2];
+if (!path) {
+  path = 'none';
+}
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-  countStudents('database.csv')
-    .then(() => res.end())
-    .catch((error) => {
-      res.status(500).send(error.message);
+  countStudents(path)
+    .then((data) => {
+      res.send(`This is the list of our students\n${data}`);
+    })
+    .catch((err) => {
+      res.send(`This is the list of our students\n${err.message}`);
     });
 });
 
