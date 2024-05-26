2c5ea15ab40b2ef66930746958d62b86705e77fb
esianyo <esianicd@gmail.com>
2024-05-26 16:49:19 +0000

    signup user

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,5 @@
 function signUpUser(firstName, lastName) {
-    return Promise.resolve({
-      firstName,
-      lastName,
-    });
-  }
-  
-  export default signUpUser;
+  return Promise.resolve({ firstName, lastName });
+}
+
+export default signUpUser;
