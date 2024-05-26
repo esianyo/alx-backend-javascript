689ed030e4858091b93aaa9b4066c840f536a231
esianyo <esianicd@gmail.com>
2024-05-26 16:40:10 +0000

    catch me if you can

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,13 @@
-  function handleResponseFromAPI(promise) {
-    const body = { status: 200, body: 'success' };
-  
-    return promise
-      .then(() => body)
-      .catch((error) => ({ status: 500, body: error.message || 'error' }))
-      .finally(() => console.log('Got a response from the API'));
-  }
-  
-  export default handleResponseFromAPI;
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
+}
+
+export default handleResponseFromAPI;
