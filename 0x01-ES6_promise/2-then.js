faab4c8b2a8cc6d664b55efbb84b3f4a0f6c04e6
esianyo <esianicd@gmail.com>
2024-05-26 16:34:20 +0000

    debugging

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,9 @@
-function handleResponseFromAPI(promise) {
+  function handleResponseFromAPI(promise) {
     const body = { status: 200, body: 'success' };
   
     return promise
       .then(() => body)
-      .catch((error) => error)
+      .catch((error) => ({ status: 500, body: error.message || 'error' }))
       .finally(() => console.log('Got a response from the API'));
   }
   
