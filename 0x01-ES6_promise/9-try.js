82875b0790233578eb2b13768204200b99756bf5
esianyo <esianicd@gmail.com>
2024-05-26 17:22:22 +0000

    ESlint

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,18 +1,18 @@
 export default function guardrail(mathFunction) {
-    // Create an empty array to store values and errors
-    const queue = [];
-  
-    try {
-      // Execute the mathFunction and append its result to the queue
-      queue.push(mathFunction());
-    } catch (error) {
-      // If an error occurs, append the error message to the queue
-      queue.push(error.message);
-    } finally {
-      // In every case, append the message "Guardrail was processed" to the queue
-      queue.push('Guardrail was processed');
-    }
-  
-    // Return the queue array
-    return queue;
+  // Create an empty array to store values and errors
+  const queue = [];
+
+  try {
+    // Execute the mathFunction and append its result to the queue
+    queue.push(mathFunction());
+  } catch (error) {
+    // If an error occurs, append the error message to the queue
+    queue.push(error.message);
+  } finally {
+    // In every case, append the message "Guardrail was processed" to the queue
+    queue.push('Guardrail was processed');
   }
+
+  // Return the queue array
+  return queue;
+}
